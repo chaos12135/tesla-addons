889f2311f9b579d63388e80e0056874837648d9d
Justin Phelps <chaos12135@users.noreply.github.com>
2022-05-03 16:06:12 -0700

    Create wdo-checkout.js

---
+++ wdo-checkout.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         Checkout Tab
+// @version      0.2
+// @description  Brings back checkout tab
+// @author       Justin
+// @match        https://warpdrive.teslamotors.com/deliveryops*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+
+function AddNewTab() {
+    setTimeout(function () {
+        var menulink = document.getElementsByClassName("top-menu")[0];
+
+        if (menulink != null) {
+            var navmenu = menulink.getElementsByTagName("nav")[0];
+            var divnavmenu = navmenu.getElementsByTagName("div")[7];
+            var menulinkvar = ('menu-link ' + divnavmenu.getAttributeNames()[0]);
+            console.log(menulinkvar);
+
+            let buttonnode = document.createElement("a");
+            buttonnode.setAttribute(divnavmenu.getAttributeNames()[0], "");
+            buttonnode.setAttribute("class", menulinkvar);
+            buttonnode.setAttribute("routerlinkactive", "active-link");
+            buttonnode.setAttribute("href", "/deliveryops/checkout?stopredirect_please=1");
+
+            let buttonnode2 = document.createElement("i");
+            buttonnode2.setAttribute(divnavmenu.getAttributeNames()[0], "");
+            buttonnode2.setAttribute("class", "checkout");
+
+            let buttonnode3 = document.createElement("span");
+            buttonnode3.setAttribute(divnavmenu.getAttributeNames()[0], "");
+            buttonnode3.setAttribute("class", "link-title");
+            let buttonnodetext3 = document.createTextNode("Checkout");
+
+            buttonnode.appendChild(buttonnode2);
+            buttonnode3.appendChild(buttonnodetext3);
+            buttonnode.appendChild(buttonnode3);
+            divnavmenu.appendChild(buttonnode);
+        } else {
+            AddNewTab();
+        };
+    }, 1000);
+};
+
+(function() {
+    AddNewTab();
+})();
