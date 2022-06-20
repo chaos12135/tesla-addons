554eb7b4b5c4141551c77cfb98be6e0cb5562b57
Justin Phelps <chaos12135@users.noreply.github.com>
2022-06-20 09:18:29 -0700

    Update wdo-checkout.js

--- wdo-checkout.js
+++ wdo-checkout.js
@@ -8,41 +8,6 @@
 // @grant        none
 // ==/UserScript==
 
-
-function AddNewTab() {
-    setTimeout(function () {
-        var menulink = document.getElementsByClassName("top-menu")[0];
-
-        if (menulink != null) {
-            var navmenu = menulink.getElementsByTagName("nav")[0];
-            var divnavmenu = navmenu.getElementsByTagName("div")[7];
-            var menulinkvar = ('menu-link ' + divnavmenu.getAttributeNames()[0]);
-
-            let buttonnode = document.createElement("a");
-            buttonnode.setAttribute(divnavmenu.getAttributeNames()[0], "");
-            buttonnode.setAttribute("class", menulinkvar);
-            buttonnode.setAttribute("routerlinkactive", "active-link");
-            buttonnode.setAttribute("href", "/deliveryops/checkout?stopredirect_please=1");
-
-            let buttonnode2 = document.createElement("i");
-            buttonnode2.setAttribute(divnavmenu.getAttributeNames()[0], "");
-            buttonnode2.setAttribute("class", "checkout");
-
-            let buttonnode3 = document.createElement("span");
-            buttonnode3.setAttribute(divnavmenu.getAttributeNames()[0], "");
-            buttonnode3.setAttribute("class", "link-title");
-            let buttonnodetext3 = document.createTextNode("Checkout");
-
-            buttonnode.appendChild(buttonnode2);
-            buttonnode3.appendChild(buttonnodetext3);
-            buttonnode.appendChild(buttonnode3);
-            divnavmenu.appendChild(buttonnode);
-        } else {
-            AddNewTab();
-        };
-    }, 1000);
-};
-
 (function() {
-    AddNewTab();
+    console.log('Why would I keep supporting this when Tesla fired me?');
 })();
