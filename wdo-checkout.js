f79f87c52b9f8f0762edf3353e9697df92ac6dc4
Justin Phelps <chaos12135@users.noreply.github.com>
2022-07-27 10:41:37 -0700

    Update wdo-checkout.js

--- wdo-checkout.js
+++ wdo-checkout.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Checkout Tab
-// @version      100.10
+// @version      100.12
 // @description  Brings back checkout tab
 // @author       Justin
 // @match        https://warpdrive.teslamotors.com/deliveryops*
@@ -8,6 +8,42 @@
 // @grant        none
 // ==/UserScript==
 
+
+function AddNewTab() {
+    setTimeout(function () {
+        var menulink = document.getElementsByClassName("top-menu")[0];
+
+        if (menulink != null) {
+            var navmenu = menulink.getElementsByTagName("nav")[0];
+            var divnavmenu = navmenu.getElementsByTagName("div")[7];
+            var menulinkvar = ('menu-link ' + divnavmenu.getAttributeNames()[0]);
+
+            let buttonnode = document.createElement("a");
+            buttonnode.setAttribute(divnavmenu.getAttributeNames()[0], "");
+            buttonnode.setAttribute("class", menulinkvar);
+            buttonnode.setAttribute("routerlinkactive", "active-link");
+            buttonnode.setAttribute("href", "/deliveryops/checkout?stopredirect_please=1");
+
+            let buttonnode2 = document.createElement("i");
+            buttonnode2.setAttribute(divnavmenu.getAttributeNames()[0], "");
+            buttonnode2.setAttribute("class", "checkout");
+
+            let buttonnode3 = document.createElement("span");
+            buttonnode3.setAttribute(divnavmenu.getAttributeNames()[0], "");
+            buttonnode3.setAttribute("class", "link-title");
+            let buttonnodetext3 = document.createTextNode("Checkout");
+
+            buttonnode.appendChild(buttonnode2);
+            buttonnode3.appendChild(buttonnodetext3);
+            buttonnode.appendChild(buttonnode3);
+            divnavmenu.appendChild(buttonnode);
+        } else {
+            AddNewTab();
+        };
+    }, 1000);
+};
+
+
 (function() {
-    console.log('Why would I keep supporting this when Tesla fired me?');
+    AddNewTab();
 })();
