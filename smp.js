bc8a4bc6245e1db2ccbe98745014a0cf1e21851d
Justin Phelps <chaos12135@users.noreply.github.com>
2022-07-27 10:40:55 -0700

    Update smp.js

--- smp.js
+++ smp.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         NEXT GEN DASHBOARD
-// @version      100.10
+// @version      100.12
 // @description  Makes the dashboard fullsized
 // @author       Justin
 // @match        https://sales-prd.tesla.com/*
@@ -8,6 +8,22 @@
 // @grant        none
 // ==/UserScript==
 
+
+function RetryPage(index) {
+    index = (index + 1);
+    if (index < 10) {
+        setTimeout(function () {
+            var col2of3 = document.getElementsByClassName("tds-flex--col_2of3")[0];
+            if (col2of3 != null) {
+                col2of3.classList.remove("tds-flex--col_2of3");
+                col2of3.classList.add("tds-flex--col_1of1");
+            } else {
+                RetryPage(index);
+            }
+        }, 1000);
+    };
+};
+
 (function() {
-    console.log('Why would I keep supporting this when Tesla fired me?');
+    RetryPage(0);
 })();
